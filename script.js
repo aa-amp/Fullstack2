a86eb94e5af6b73bfefc14d77460582eacf9704b
aa-amp <aa.ampuero@duocuc.cl>
2025-09-01 19:16:03 -0400

    Inicio pagina principal
    Listo el carrusel

---
+++ script.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carrusel-track');
+const btnPrev = document.querySelector('.carrusel-btn.prev');
+const btnNext = document.querySelector('.carrusel-btn.next');
+const cards = document.querySelectorAll('.producto-card');
+
+let index = 0;
+const cardWidth = 400; // igual que en CSS
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * cardWidth}px)`;
+}
+
+btnNext.addEventListener('click', () => {
+  index++;
+  if (index >= cards.length) index = 0;
+  updateCarousel();
+});
+
+btnPrev.addEventListener('click', () => {
+  index--;
+  if (index < 0) index = cards.length - 1;
+  updateCarousel();
+});
